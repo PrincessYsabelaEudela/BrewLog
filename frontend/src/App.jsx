d7f9fc21806483ba3e8724283b0cc1c8cda03f9a
Princess Ysabela Eudela <ela.eudela@gmail.com>
2026-01-18 14:49:07 +0400

    react-router-dom installation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
 
 const App = () => {
   return (
-    <div className='text-3xl font-bold underline'>
-      App
-    </div>
-  )
-}
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
