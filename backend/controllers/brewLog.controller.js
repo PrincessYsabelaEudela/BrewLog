53fcf782fc83a73e6553cbf8d19610cb46177920
Princess Ysabela Eudela <ela.eudela@gmail.com>
2026-01-20 14:37:24 +0400

    delete image

--- backend/controllers/brewLog.controller.js
+++ backend/controllers/brewLog.controller.js
@@ -1,5 +1,8 @@
 import BrewLog from '../models/brewLog.model.js';
 import { errorHandler } from '../utils/error.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
 
 export const addBrewLog = async (req, res, next) => {
   const { title, review, tags, imageUrl } = req.body;
@@ -49,8 +52,6 @@ export const getAllBrewLogs = async (req, res, next) => {
   }
 };
 
-export const getAllTravelStory = async (req, res, next) => {};
-
 export const imageUpload = async (req, res, next) => {
   try {
     if (!req.file) {
@@ -65,7 +66,37 @@ export const imageUpload = async (req, res, next) => {
   }
 };
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// const rootDir = path.join(__dirname, '..');
+const rootDir = path.join(__dirname, '..');
+
+export const deleteImage = async (req, res, next) => {
+  const { imageUrl } = req.query;
+
+  if (!imageUrl) {
+    return next(errorHandler(400, 'imageUrl parameter is required!'));
+  }
+
+  try {
+    // extract the file name from the imageUrl
+    const filename = path.basename(imageUrl);
+
+    // Delete the file path
+    const filePath = path.join(rootDir, 'uploads', filename);
+
+    console.log(filePath);
+
+    // check if the file exists
+    if (!fs.existsSync(filePath)) {
+      return next(errorHandler(404, 'Image not found!'));
+    }
+
+    // delete the file
+    await fs.promises.unlink(filePath);
+
+    res.status(200).json({ message: 'Image deleted successfully!' });
+  } catch (error) {
+    next(error);
+  }
+};
--- backend/routes/brewLog.route.js
+++ backend/routes/brewLog.route.js
@@ -4,12 +4,15 @@ import {
   addBrewLog,
   getAllBrewLogs,
   imageUpload,
+  deleteImage,
 } from '../controllers/brewLog.controller.js';
 import upload from '../multer.js';
 
 const router = express.Router();
 
 router.post('/image-upload', upload.single('image'), imageUpload);
+
+router.delete('/delete-image', deleteImage);
 
 router.post('/add', verifyToken, addBrewLog);
 
