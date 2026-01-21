4ae01f4583e32ea67e4476ab411739936dc2dd70
Princess Ysabela Eudela <ela.eudela@gmail.com>
2026-01-21 15:57:58 +0400

    get all reviews

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../../components/Navbar';
+import axiosInstance from '../../utils/axiosInstance';
 
 const Home = () => {
+  const [allReviews, setAllReviews] = useState([]);
+
+  console.log(allReviews);
+
+  // Get all travel stories
+  const getAllBrewLogs = async () => {
+    try {
+      const response = await axiosInstance.get('/brewlog/get-all');
+
+      if (response.data && response.data.reviews) {
+        setAllReviews(response.data.reviews);
+      }
+    } catch (error) {
+      console.log('Something went wrong. Please try again.');
+    }
+  };
+
+  useEffect(() => {
+    getAllBrewLogs();
+
+    return () => {};
+  }, []);
+
   return (
     <>
       <Navbar />
+
+      <div className="container mx-auto py-10">
+        <div className="flex gap-7">
+          <div className="flex-1"> </div>
+
+          <div className="w-[320px]"> </div>
+        </div>
+      </div>
     </>
   );
 };
