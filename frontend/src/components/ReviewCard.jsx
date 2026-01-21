1b217a8ce4c26a6e68a6afeaea8e60de3bc97b5d
Princess Ysabela Eudela <ela.eudela@gmail.com>
2026-01-21 16:44:48 +0400

    review component

---
+++ frontend/src/components/ReviewCard.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { FaHeart } from 'react-icons/fa';
+
+const ReviewCard = ({
+  imageUrl,
+  title,
+  review,
+  tags,
+  isFavourite,
+  onEdit,
+  onClick,
+  onFavouriteClick,
+}) => {
+  return (
+    <div className="border border-slate-200 rounded-lg overflow-hidden bg-white hover:shadow-lg hover:shadow-slate-200 transition-all ease-in-out relative cursor-pointer">
+      <img
+        src={imageUrl}
+        alt={title}
+        className="w-full h-100 object-cover rounded-lg"
+        onClick={onClick}
+      />
+
+      <button
+        className="w-12 h-12 flex items-center justify-center bg-white/40 rounded-lg border border-white/30 absolute top-4 right-4"
+        onClick={onFavouriteClick}
+      >
+        <FaHeart
+          className={`icon-btn ${
+            isFavourite ? 'text-accent' : 'text-white'
+          } hover:text-accent cursor-pointer`}
+        />
+      </button>
+
+      <div className="p-4" onClick={onClick}>
+        <div className="flex items-center gap-3">
+          <div className="flex-1">
+            <h6 className="text-[16px] font-medium">{title}</h6>
+          </div>
+        </div>
+
+        <p className="text-sm text-slate-600 mt-2">{review?.slice(0, 60)}</p>
+
+        <div className="inline-flex items-center gap-2 text-[13px] rounded py-1">
+          {tags?.map((tag, index) => (
+            <span key={index} className="px-2 py-1 bg-accent/30 rounded">
+              {tag}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewCard;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,19 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from '../../components/Navbar';
 import axiosInstance from '../../utils/axiosInstance';
+import ReviewCard from '../../components/ReviewCard';
+import EmptyCard from '../../components/EmptyCard';
 
 const Home = () => {
   const [allReviews, setAllReviews] = useState([]);
 
   console.log(allReviews);
 
-  // Get all travel stories
+  // Get all reviews
   const getAllBrewLogs = async () => {
     try {
       const response = await axiosInstance.get('/brewlog/get-all');
 
       if (response.data && response.data.reviews) {
         setAllReviews(response.data.reviews);
+      }
+    } catch (error) {
+      console.log('Something went wrong. Please try again.');
+    }
+  };
+
+  // Handle Edit review
+  const handleEdit = async (data) => {
+    setOpenAddEditModal({ isShown: true, type: 'edit', data: data });
+  };
+
+  const handleViewReview = (data) => {
+    setOpenViewModal({ isShown: true, data });
+  };
+
+  const addToFavourite = async (storyData) => {
+    const storyId = storyData._id;
+
+    try {
+      const response = await axiosInstance.put(
+        '/brewlog/add-to-favourite/' + storyId,
+        {
+          isFavorite: !storyData.isFavorite,
+        },
+      );
+
+      if (response.data && response.data.story) {
+        toast.success('Story updated successfully!');
+        getAllBrewLogs();
       }
     } catch (error) {
       console.log('Something went wrong. Please try again.');
@@ -32,9 +63,41 @@ const Home = () => {
 
       <div className="container mx-auto py-10">
         <div className="flex gap-7">
-          <div className="flex-1"> </div>
-
-          <div className="w-[320px]"> </div>
+          <div className="flex-1">
+            {allReviews.length > 0 ? (
+              <div className="grid grid-cols-4 gap-4">
+                {allReviews.map((item) => {
+                  return (
+                    <ReviewCard
+                      key={item._id}
+                      imageUrl={item.imageUrl}
+                      title={item.title}
+                      review={item.review}
+                      tags={item.tags}
+                      isFavourite={item.isFavorite}
+                      onEdit={() => handleEdit(item)}
+                      onClick={() => handleViewReview(item)}
+                      onFavouriteClick={() => addToFavourite(item)}
+                    />
+                  );
+                })}
+              </div>
+            ) : (
+              <EmptyCard
+                imgSrc={
+                  'https://images.pexels.com/photos/5706021/pexels-photo-5706021.jpeg?auto=compress&cs=tinysrgb&w=600'
+                }
+                // message={getEmptyCardMessage(filterType)}
+                // setOpenAddEditModal={() =>
+                //   setOpenAddEditModal({
+                //     isShown: true,
+                //     type: 'add',
+                //     data: null,
+                //   })
+                // }
+              />
+            )}
+          </div>
         </div>
       </div>
     </>
